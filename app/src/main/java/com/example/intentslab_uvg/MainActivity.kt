ddfe9eea1db1e0247bad5716dc17281c89986468
MGonza20 <64711979+MGonza20@users.noreply.github.com>
2021-05-19 18:14:34 -0600

    Se terminaron los diseños xml, falta un poco de implementación de código

--- app/src/main/java/com/example/intentslab_uvg/MainActivity.kt
+++ app/src/main/java/com/example/intentslab_uvg/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.intentslab_uvg
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,13 +11,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnOpen.setOnClickListener{
+        btnSuma.setOnClickListener{
+
+            /** INFO VALIOSA
             val intent: Intent = Intent(this, MainActivity2::class.java)
-            intent.putExtra("valor", "Hola")
-            startActivity(intent)
+
+            intent.putExtra("valor", "Hola mundo")
+            intent.putExtra("valor2", "Prueba")
+            **/
+
+            //intent.putExtra("valor3", "datos random")
+            //startActivity(intent)
             //finish()
+
+            startActivityForResult(intent, 1)
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
+        if(resultCode == RESULT_OK){
+            if(requestCode == 1){
+                val bundle = data?.extras
+                if(bundle != null){
+                    val resultado = bundle.getString("resultado1")
+                    Toast.makeText(this, resultado, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/intentslab_uvg/MainActivity2.kt
+++ app/src/main/java/com/example/intentslab_uvg/MainActivity2.kt
@@ -1,19 +1,60 @@
 package com.example.intentslab_uvg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main2.*
+import kotlinx.android.synthetic.main.activity_main2.etValor1
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        /** INFO FUNCIONAL
         val bundle = intent.extras
 
         if(bundle != null){
             val texto = bundle.getString("valor")
-            txtTexto.text = texto
+            tvResultado.text = texto
+
+            val valor2 = bundle.getString("valor2")
+            Toast.makeText(this, valor2, Toast.LENGTH_LONG).show()
+        **/
+
+        val valor1 : Int
+        val valor2 : Int
+
+        btnRegresar.setOnClickListener {
+            val intent: Intent = Intent(this, MainActivity::class.java)
+
+            
+
+            intent.putExtra("valor", "Hola mundo")
+            intent.putExtra("valor2", "Prueba")
         }
+
+/**
+            val valor3 = bundle.getString("valor3", "No hay valor")
+            Toast.makeText(this, valor3, Toast.LENGTH_LONG).show()
+**/
+
+            /**
+            if(valor3 != null){
+                Toast.makeText(this, valor3, Toast.LENGTH_LONG).show()
+            }
+
+        }
+             **/
+
+        btnRegresar.setOnClickListener{
+            val intent: Intent = Intent()
+            intent.putExtra("resultado1", "Valor desde pantalla 2")
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+
     }
 }
